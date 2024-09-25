3638dbd45c1cf4b1eca15ae0c3b46b50643bcafa
Thibaut <tmilville.pro@gmail.com>
2024-09-25 22:28:25 +0200

    Initial config

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { Anek_Telugu } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const AnekTelugu = Anek_Telugu({subsets: ["latin"], variable: "--font-caption"});
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Thibaut MILVILLE",
+  description: "Portfolio of Thibaut MILVILLE",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body
+        className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
