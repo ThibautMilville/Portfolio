71c51ac34d4eced2539915e74804eeb5f1f7538f
Thibaut <tmilville.pro@gmail.com>
2024-09-30 22:17:11 +0200

    Design and structure improvements

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full")}
+        className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground")}
       >
         {children}
       </body>
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Header } from "./_components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
+      <Header />
     </main>
   );
 }
