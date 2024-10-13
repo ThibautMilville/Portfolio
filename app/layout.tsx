0a2594df3de329215201c30dd6b9f77cee1f32dc
Thibaut <tmilville.pro@gmail.com>
2024-10-14 00:44:36 +0200

    Test deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 /* import localFont from "next/font/local"; */
 import "./globals.css";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { Anek_Telugu } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 const AnekTelugu = Anek_Telugu({ subsets: ["latin"], variable: "--font-caption" });
 
-/* const geistSans = localFont({
+const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
@@ -17,7 +15,7 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-}); */
+});
 
 export const metadata: Metadata = {
   title: "Thibaut MILVILLE",
@@ -31,7 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn(GeistSans.variable, GeistMono.variable, AnekTelugu.variable, "font-sans h-full bg-background text-foreground")}>{children}</body>
+      <body className={cn(geistSans.variable, geistMono.variable, AnekTelugu.variable, "font-sans h-full bg-background text-foreground")}>{children}</body>
     </html>
   );
+}
+
+function localFont({ src, variable, weight }: { src: string; variable: string; weight: string; }) {
+  return { variable };
 }
