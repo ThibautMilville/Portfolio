51077912f9b1eae8256afe43e5d9b6d671c643f0
Thibaut <tmilville.pro@gmail.com>
2024-10-14 22:11:34 +0200

    Local images imports

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 py-4">
       <Section className="flex items-baseline">
-        <h1 className="text-lg font-bold text-primary">thibautmilville.com</h1>
+        <h1 className="text-lg font-bold text-primary">Portfolio</h1>
         <div className="flex-1" />
         <ul className="flex items-center gap-2">
           <Link href="https://www.linkedin.com/in/thibaut-milville/" className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}>
