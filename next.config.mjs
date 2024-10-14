ac2205564fe0a945f49e314c4965bf376f7ee4b1
Thibaut <tmilville.pro@gmail.com>
2024-10-14 21:30:20 +0200

    Test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.linkedin.com", "https://media.licdn.com/"],
+    domains: ["linkedin.com", "media.licdn.com"],
   },
 };
 
